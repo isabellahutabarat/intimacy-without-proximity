f02579eb3d5facdd02c05a8bf0a36981d5b17e78
isabellahutabarat <hutai493@newschool.edu>
2025-12-08 23:49:07 -0500

    Mobile improvements: more line spacing, lighter lines, URL wrapping, aligned URLs on mobile

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ window.CONFIG = {
   },
   spacing: {
     pagePaddingX: 18, // px (also used vertically)
-    rowPaddingY: 7, // px from lines to top/bottom of text
+    rowPaddingY: 9, // px from lines to top/bottom of text (increased for more breathing room)
     cellPaddingX: 12, // px horizontal padding between date/title/author
     gapDateTitlePx: 87, // exact gap between Date and Title
     gapTitleAuthorPx: 150, // space between Title and Author
@@ -24,7 +24,7 @@ window.CONFIG = {
     offsetDatePx: 0, // add left offset for Date column without changing width
     offsetTitlePx: 25, // add left offset for Title column without changing width
     offsetAuthorPx: 180, // add left offset for Author column without changing Title width
-    lineWidthPx: 1
+    lineWidthPx: 0.5 // Lighter line weight
   },
   layout: {
     aboutWidthVw: 25, // percent of viewport widthno
